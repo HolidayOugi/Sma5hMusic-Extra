968f9afdeaa6723de7fff9f5d236c70ba562d5da
Ougi <emyquaranta@gmail.com>
2026-07-12 11:34:13 +0200

    It lost the comment in the commit???

--- Sma5h/Mods/Sma5h.Mods.Music/CskPackBuild/CskPackBuildService.Files.cs
+++ Sma5h/Mods/Sma5h.Mods.Music/CskPackBuild/CskPackBuildService.Files.cs
@@ -88,6 +88,7 @@ namespace Sma5h.Mods.Music.CskPackBuild
             foreach (var entry in entries)
             {
                 var source = Path.Combine(generatedBgmFolder, string.Format(MusicConstants.GameResources.NUS3BANK_FILE, entry.NameId));
+                //check nus3audio exists before copying (ie replacement song)
                 var audioSource = Path.Combine(generatedBgmFolder, string.Format(MusicConstants.GameResources.NUS3AUDIO_FILE, entry.NameId));
                 if (!File.Exists(source) || File.Exists(audioSource))
                     continue;
