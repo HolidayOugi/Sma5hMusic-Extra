44528be2997911f7c1959431a620b09b6946f755
Ougi <emyquaranta@gmail.com>
2026-07-19 11:47:19 +0200

    Cap console text rendering to 1000 lines

--- Sma5hMusic.GUI/Helpers/ConsoleWriter.cs
+++ Sma5hMusic.GUI/Helpers/ConsoleWriter.cs
@@ -2,6 +2,7 @@
 using Avalonia.Threading;
 using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Threading;
@@ -11,9 +12,12 @@ namespace Sma5hMusic.GUI.Helpers
 {
     public class ControlWriter : TextWriter
     {
+        private const int MaxDisplayedLines = 1000;
         private readonly TextBox _textbox;
         private readonly ScrollViewer _scrollViewer;
         private readonly ConcurrentQueue<string> _queuedMessages;
+        private readonly Queue<string> _displayedLines;
+        private readonly StringBuilder _currentDisplayedLine;
         private readonly CancellationTokenSource _cts;
 
         public ControlWriter(TextBox textbox, ScrollViewer scrollViewer)
@@ -21,6 +25,8 @@ namespace Sma5hMusic.GUI.Helpers
             _textbox = textbox;
             _scrollViewer = scrollViewer;
             _queuedMessages = new ConcurrentQueue<string>();
+            _displayedLines = new Queue<string>();
+            _currentDisplayedLine = new StringBuilder();
             _cts = new CancellationTokenSource();
             _ = Task.Run(async() => await RunQueue(_cts.Token));
         }
@@ -45,18 +51,44 @@ namespace Sma5hMusic.GUI.Helpers
         {
             while (!cancellationToken.IsCancellationRequested)
             {
-                var messagesToPrint = string.Empty;
+                var messagesToPrint = new StringBuilder();
                 int i = 0;
                 while (i < 100 && _queuedMessages.TryDequeue(out string newMessage))
                 {
-                    messagesToPrint += newMessage;
+                    messagesToPrint.Append(newMessage);
                     i++;
                 }
-                if (!string.IsNullOrEmpty(messagesToPrint))
+                if (messagesToPrint.Length > 0)
                 {
+                    var textToPrint = messagesToPrint.ToString();
                     await Dispatcher.UIThread.InvokeAsync(() =>
                     {
-                        _textbox.Text += messagesToPrint;
+                        foreach (var character in textToPrint)
+                        {
+                            //ignore Windows line ending, might screw up the line
+                            if (character == '\r')
+                                continue;
+
+                            if (character == '\n')
+                            {
+                                _displayedLines.Enqueue(_currentDisplayedLine + "\r\n");
+                                _currentDisplayedLine.Clear();
+                                continue;
+                            }
+
+                            _currentDisplayedLine.Append(character);
+                        }
+
+                        //FIFO queue for lines
+                        while (_displayedLines.Count > MaxDisplayedLines)
+                            _displayedLines.Dequeue();
+
+                        var displayedText = new StringBuilder();
+                        foreach (var line in _displayedLines)
+                            displayedText.Append(line);
+                        displayedText.Append(_currentDisplayedLine);
+
+                        _textbox.Text = displayedText.ToString();
                         _scrollViewer.ScrollToEnd();
                     }, DispatcherPriority.Background);
                 }
